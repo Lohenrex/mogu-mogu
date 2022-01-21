c210591bd8f4cd3a51a55279d1618365e798ff5a
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-21 12:16:40 -0600

    Setup mail configuration

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              =>  'smtp.sendgrid.net',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>  'app246321565@heroku.com',
+  :password             =>  'rf3t6s8l0251',
+  :domain               =>  'heroku.com',
+  :enable_starttls_auto  =>  true
+}
