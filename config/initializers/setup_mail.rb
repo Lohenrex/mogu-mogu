d2c0438ada89d4930a44c3678d81ceb8c12fd2d1
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-21 19:18:26 -0600

    Delete unused mail configurations

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,8 +5,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: "587",
   authentication: :plain,
-  user_name: ENV.fetch("smtp_user"),
-  password: ENV.fetch("smtp_password"),
-  domain: ENV.fetch("smtp_domain"),
+  user_name: ENV.fetch("smtp_user", "zam"),
+  password: ENV.fetch("smtp_password", "secret"),
+  domain: ENV.fetch("smtp_domain", "shrek"),
   enable_starttls_auto: true
 }
