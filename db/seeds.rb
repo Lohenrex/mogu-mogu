000f437ae2ecf37c36cc2b4c8313780e1296bae5
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-23 13:00:33 -0600

    Modify ingredients crud views and add seeds for the model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Ingredient.destroy_all
+
+Ingredient.create!([{
+  name: "whole milk",
+  category: "dairy"
+},{
+  name: "all-purpose flour",
+  category: "flour"
+},{
+  name: "cake flour",
+  category: "flour"
+},{
+  name: "egg",
+  category: "meats"
+},{
+  name: "water",
+  category: "liquids"
+},{
+  name: "strawberry",
+  category: "fruits"
+},{
+  name: "blueberry",
+  category: "fruits"
+},{
+  name: "unsalted butter",
+  category: "dairy"
+},{
+  name: "mint",
+  category: "vegetables"
+},{
+  name: "heavy whipping cream",
+  category: "dairy"
+},{
+  name: "milk (any)",
+  category: "dairy"
+},{
+  name: "vodka",
+  category: "alcohol"
+},{
+  name: "salt",
+  category: "spices"
+},{
+  name: "sugar",
+  category: "spices"
+}])
+
+p "Created #{Ingredient.count} ingredients"
