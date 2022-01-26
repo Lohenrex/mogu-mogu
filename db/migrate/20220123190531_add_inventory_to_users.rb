b824f92f01498905d68f78114740e0befdc28058
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-26 16:47:45 -0600

    Remove unecessary migrations

--- db/migrate/20220123190531_add_inventory_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddInventoryToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :inventory, :json
-  end
-end
--- db/migrate/20220125172503_remove_inventory_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveInventoryFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :inventory, :json
-  end
-end
