7f587b6ef43d99bffbe80d3a511b40d47dd9f828
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-25 19:08:28 -0600

    Remove inventory field from users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_190531) do
+ActiveRecord::Schema.define(version: 2022_01_25_182117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,22 @@ ActiveRecord::Schema.define(version: 2022_01_23_190531) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_ingredients_on_name", unique: true
+  end
+
+  create_table "ingredients_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.index ["ingredient_id", "user_id"], name: "index_ingredients_users_on_ingredient_id_and_user_id"
+    t.index ["user_id", "ingredient_id"], name: "index_ingredients_users_on_user_id_and_ingredient_id"
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_inventories_on_ingredient_id"
+    t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,10 +54,11 @@ ActiveRecord::Schema.define(version: 2022_01_23_190531) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role", default: 3, null: false
     t.string "name", null: false
-    t.json "inventory"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inventories", "ingredients"
+  add_foreign_key "inventories", "users"
 end
