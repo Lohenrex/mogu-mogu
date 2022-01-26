369dbc8738dc6bb40b5b0165af722879e7cf2db2
Lohenrex <lohenrex.ragnarok@gmail.com>
2022-01-26 11:50:36 -0600

    Remove unused format.json from usersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update destroy]
 
-  # GET /users or /users.json
+  # GET /users
   def index
     @users = User.all
   end
@@ -11,26 +11,23 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit; end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  # PATCH/PUT /users/1
   def update
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to users_url, notice: "User was successfully updated." }
-        format.json { render :index, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1 or /users/1.json
+  # DELETE /users/1
   def destroy
     @user.destroy
 
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
