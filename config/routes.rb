dcd9fe0f3796e862f24fbf45780a8e7c41fd089d
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-26 19:04:01 -0600

    Remove unused show route from ingredients resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :ingredients
+  resources :ingredients, only: %i[ index new create edit update destroy ]
   resources :ingredient_categories, only: %i[ index new create edit update destroy ]
   resources :users, only: %i[index edit update destroy]
 end
