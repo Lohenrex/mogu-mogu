f3b27cb291c8fdd2e83ad233f59980ed3a5c05ce
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-28 16:03:40 -0600

    Exclude non admins from resources views

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :ingredients, except: %i[show]
-  resources :ingredient_categories, except: %i[show]
-  resources :recipe_categories, except: %i[show]
-  resources :appliances, except: %i[show]
-  resources :users, except: %i[show new create]
+  scope "/admin" do
+    resources :ingredients, except: %i[show]
+    resources :ingredient_categories, except: %i[show]
+    resources :recipe_categories, except: %i[show]
+    resources :appliances, except: %i[show]
+    resources :users, except: %i[show new create]
+  end
 end
