c99637fe95ac4f492e671208a4c6b3f0cbb4a7c0
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-01-31 12:33:23 -0600

    Create inventory model, controller and routes

---
+++ app/controllers/inventories_controller.rb
@@ -0,0 +1,55 @@
+class InventoriesController < ApplicationController
+  before_action :set_inventory, only: %i[destroy]
+
+  # GET /inventories
+  def index
+    @inventory = Inventory.where(user_id: current_user.id)
+  end
+
+  # GET /inventories/new
+  def new
+    @ingredients_list = ingredients_for_select
+    @inventory = Inventory.new
+  end
+
+  # POST /inventories
+  def create
+    @inventory = Inventory.new(inventory_params.merge(user_id: current_user.id))
+
+    if @inventory.save
+      redirect_to inventories_url, notice: "The ingredient was successfully added to your inventory!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /inventories/1 or /inventories/1.json
+  def destroy
+    @inventory.destroy
+    redirect_to inventories_url, notice: "The ingredient was removed from your inventory."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_inventory
+      @inventory = Inventory.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def inventory_params
+      params.require(:inventory).permit(:user_id, :ingredient_id)
+    end
+
+    def ingredients_not_in_inventory
+      inventory = Inventory.where(user_id: current_user.id)
+      if inventory.empty?
+        return Ingredient.all
+      else
+        return Ingredient.where.not(id: inventory.pluck(:ingredient_id))
+      end
+    end
+
+    def ingredients_for_select
+      ingredients_not_in_inventory.sort_by(&:name).map{ |ingredient| [ingredient.name, ingredient.id] }
+    end
+end
