5f7b496e58f04e2ebfcbdd0e1ae891e33eeda8e0
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-02 16:07:09 -0600

    Add validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ApplicationRecord
   belongs_to :recipe_category
   belongs_to :user
 
+  validates :name, :description, :picture, presence: true
+
   has_one_attached :steps_video
   has_one_attached :picture do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
