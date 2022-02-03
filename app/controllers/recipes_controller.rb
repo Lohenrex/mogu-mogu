1e55e6c66eed06b4fba1a20bf0b639de2f8152a4
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-03 13:37:11 -0600

    Add functions to sanitize ingredients and appliances before storing them on the db

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,7 @@ class RecipesController < ApplicationController
   before_action :set_recipe_categories, except: %i[index]
   before_action :set_ingredients, except: %i[index]
   before_action :set_appliances, except: %i[index]
+  before_action :set_ingredients_json, only: %i[show]
 
   # GET /recipes or /recipes.json
   def index
@@ -70,19 +71,36 @@ class RecipesController < ApplicationController
     @appliances = Appliance.all
   end
 
+  def set_ingredients_json
+    ingredients_param = @recipe.ingredients
+    @ingredients_json = JSON.parse(ingredients_param)["needed"]
+  end
+
   # Only allow a list of trusted parameters through.
   def recipe_params
     step_param = params[:recipe][:steps]
+    ingredient_param = params[:recipe][:ingredients]
+    appliance_param = params[:recipe][:appliances]
     filtered_params = params.require(:recipe).permit(:name, :description, :picture,
                                                      :ingredients, :appliances, :steps,
                                                      :recipe_category_id, :steps_video, :user_id)
 
-    filtered_params.merge(steps: sanitize_steps(step_param))
+    filtered_params.merge(steps: sanitize_steps(step_param),
+                          ingredients: sanitize_appliances(appliance_param),
+                          appliances: sanitize_ingredients(ingredient_param))
 
     filtered_params
   end
 
   def sanitize_steps(param)
-    param.split("\n").compact_blank if param.present?
+    param.split("\r\n").compact_blank if param.present?
+  end
+
+  def sanitize_appliances(param)
+    param.split(",").compact_blank if param.present?
+  end
+
+  def sanitize_ingredients(param)
+    JSON.parse(param)
   end
 end
