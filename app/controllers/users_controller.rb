be857e1956a2ddeaf7a7bd6f0d0c322cde601bb3
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-02 19:03:39 -0600

    Enable pundit auth for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    @users = authorize User.all
   end
 
   # GET /users/1/edit
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    @user = User.find(params[:id])
+    @user = authorize User.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
