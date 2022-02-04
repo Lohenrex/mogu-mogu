f685c53181f49d571d11c87c0aa272a6e331ba0e
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-04 17:07:23 -0600

    Remove useless migrations and merge useful changes into their corresponding migration files

--- db/migrate/20220121191654_remove_details_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDetailsFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    ## Confirmable
-    remove_columns(:users, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email)
-  end
-end
--- db/migrate/20220122002737_alter_name_in_users.rb
+++
@@ -1,5 +0,0 @@
-class AlterNameInUsers < ActiveRecord::Migration[7.0]
-  def change    
-    add_index :users, :name, unique: true
-  end
-end
