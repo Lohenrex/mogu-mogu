7c280ac96bb20125622e38e0eba8e7870a1554f8
Lohenrex <lohenrex.ragnarok@gmail.com>
2022-02-09 13:37:18 -0600

    [Feature] Add community recipes browser (#25)
    * Add GIN index to recipes.ingredients
    
    * Add bulma-carousel
    
    * Create view for the oracle feature
    
    * Add more ingredient seeds
    
    * Filter recipes by existing ingredient on home_controller for the oracle feature
    
    * Adjust sanitizing methods according to the new ingredients JSON
    
    * Append oracle view to welcome view
    
    * Remove carousel and use multiline columns instead
    
    * Refactor home_controller by sending before_action methods to the index method
    
    * Refactor redundant scope
    
    * Rubify condition when there are no recipes for the oracle
    
    * Let rubocop do its thing
    
    * Create partial view to show the 3 latest recipes
    
    * Amend recipes_controller to decide which recipes are to be rendered
    
    * Amend recipes views to improve UI
    
    * Refactor index method in home_controller to reduce complexity
    
    * Define an explicit assignment to doable_recipes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,17 @@ class HomeController < ApplicationController
     return unless user_signed_in?
 
     @user_ingredients = current_user.inventory.map { |item| item.ingredient.name }
+    @latest_recipes = Recipe.last(3)
+    @doable_recipes = find_doable_recipes
+  end
 
+  private
+
+  def find_doable_recipes
     doable_recipes_ids = @user_ingredients.map do |ingredient|
       Recipe.with_ingredient(ingredient).pluck(:id)
     end.flatten.uniq
 
-    @doable_recipes = Recipe.find(doable_recipes_ids)
+    Recipe.find(doable_recipes_ids)
   end
 end
