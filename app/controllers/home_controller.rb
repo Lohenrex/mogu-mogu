eb73dd7c00ac8a33fdad9e70c53cbcfb88ad403d
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-09 13:10:32 -0600

    Refactor index method in home_controller to reduce complexity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,17 @@ class HomeController < ApplicationController
     return unless user_signed_in?
 
     @user_ingredients = current_user.inventory.map { |item| item.ingredient.name }
+    @latest_recipes = Recipe.last(3)
+    set_doable_recipes
+  end
 
+  private
+
+  def set_doable_recipes
     doable_recipes_ids = @user_ingredients.map do |ingredient|
       Recipe.with_ingredient(ingredient).pluck(:id)
     end.flatten.uniq
 
     @doable_recipes = Recipe.find(doable_recipes_ids)
-
-    @latest_recipes = Recipe.all.order(:updated_at).reverse_order.take(3)
   end
 end
