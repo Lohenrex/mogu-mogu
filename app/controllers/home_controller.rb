bd2288ae6c1421ca87c4b3d1b3fc4105f04cd94f
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-08 18:20:32 -0600

    Filter recipes by existing ingredient on home_controller for the oracle feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :set_user_ingredients, only: %i[index]
+  before_action :set_doable_recipes, only: %i[index]
+
   def index; end
+
+  private
+
+  def set_user_ingredients
+    @user_ingredients = current_user.inventory.map { |item| item.ingredient.name } if user_signed_in?
+  end
+
+  def set_doable_recipes
+    return false unless user_signed_in?
+
+    doable_recipes_ids = @user_ingredients.map do |ingredient|
+      Recipe.get_recipe_which_has_ingredient(ingredient).pluck(:id)
+    end.flatten.uniq
+
+    @doable_recipes = Recipe.find(doable_recipes_ids)
+  end
 end
