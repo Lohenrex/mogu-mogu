7c763b6c09e34a63bd64886af93c19fa5ad5ea86
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-08 18:22:11 -0600

    Append oracle view to welcome view

--- app/javascript/custom/home.js
+++ app/javascript/custom/home.js
@@ -1,3 +1,19 @@
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
+// Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach('.carousel', carouselOptions);
+
+const carouselOptions = {
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  infinite: true,
+  autoplay: true,
+  pagination: false,
+}
+
+document.querySelector(".slider-container").style.width = "auto";
+document.querySelectorAll(".slider-item").forEach(item => { item.style.width = "auto"; });
+
 const toggleBurger = () => {
   let burgerIcon = document.querySelector('.burger');
   let dropMenu = document.querySelector('#navbarMenu');
