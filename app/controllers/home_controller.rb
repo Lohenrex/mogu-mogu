cfbca511cb3f126c093477e3aaee4035130abfe3
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-08 19:13:42 -0600

    Refactor home_controller by sending before_action methods to the index method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :set_user_ingredients, only: %i[index]
-  before_action :set_doable_recipes, only: %i[index]
+  def index
+    return unless user_signed_in?
 
-  def index; end
-
-  private
-
-  def set_user_ingredients
-    @user_ingredients = current_user.inventory.map { |item| item.ingredient.name } if user_signed_in?
-  end
-
-  def set_doable_recipes
-    return false unless user_signed_in?
+    @user_ingredients = current_user.inventory.map { |item| item.ingredient.name }
 
     doable_recipes_ids = @user_ingredients.map do |ingredient|
       Recipe.get_recipe_which_has_ingredient(ingredient).pluck(:id)
