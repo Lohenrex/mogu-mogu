3d306b08d9554a02c6eb43ac482db181dbe050b3
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2022-02-10 16:49:27 -0600

    Add js functions to preload existing appliances and ingredients in the recipe edit view

--- app/javascript/custom/recipes.js
+++ app/javascript/custom/recipes.js
@@ -1,59 +1,99 @@
-const pictureInput = document.querySelector('#recipe_picture');
-const stepsVideoInput = document.querySelector('#recipe_steps_video');
-const ingredientAdder = document.querySelector('#ingredient_adder');
-const applianceAdder = document.querySelector('#appliance_adder');
-const ingredientSelect = document.querySelector("#recipe_ingredient_select");
-const ingredientAmount = document.querySelector("#recipe_ingredient_amount");
-const ingredientMeasure = document.querySelector("#recipe_ingredient_measure");
-const applianceInput = document.querySelector("#recipe_appliance_selector");
-const theForm = document.querySelector("#recipesForm");
+document.addEventListener("turbo:load", () => {
+  const pictureInput = document.querySelector('#recipe_picture');
+  const stepsVideoInput = document.querySelector('#recipe_steps_video');
+  const ingredientAdder = document.querySelector('#ingredient_adder');
+  const applianceAdder = document.querySelector('#appliance_adder');
+  const ingredientsInput = document.querySelector("#recipe_ingredients");
+  const appliancesInput = document.querySelector("#recipe_appliances");
+  const ingredientSelect = document.querySelector("#recipe_ingredient_select");
+  const ingredientAmount = document.querySelector("#recipe_ingredient_amount");
+  const ingredientMeasure = document.querySelector("#recipe_ingredient_measure");
+  const applianceSelector = document.querySelector("#recipe_appliance_selector");
+  const isEditPath = document.querySelector("#current_recipe_path");
+  const theForm = document.querySelector("#recipesForm");
+  let selectedIngredients = [];
+  let selectedAppliances = [];
 
-let selectedIngredients = [];
-let selectedAppliances = [];
-
-pictureInput.onchange = () => {
-  if (pictureInput.files.length > 0) {
-    const fileName = pictureInput.parentNode.querySelector(".file-name");
-    fileName.textContent = pictureInput.files[0].name;
+  const appendIngredientToArray = (newName, newAmount, newMeasure) => {
+    let newIngredient = {name:newName, quantity:newAmount, measure:newMeasure };
+    selectedIngredients.push(newIngredient);  
   }
-}
 
-stepsVideoInput.onchange = () => {
-  if (stepsVideoInput.files.length > 0) {
-    const fileName = stepsVideoInput.parentNode.querySelector(".file-name");
-    fileName.textContent = stepsVideoInput.files[0].name;
+  const appendApplianceToArray = (newAppliance) => { selectedAppliances.push(newAppliance); }
+
+  const loadAppliances = () => {
+    currentAppliances.forEach(applianceName => { 
+      appendApplianceToArray(applianceName);
+      appendApplianceToView(applianceName);
+    });
   }
-}
 
-ingredientAdder.addEventListener("click", (e) => {
-  appendIngredientToArray();
-  appendIngredientToView();
+  const loadIngredients = () => {
+    let parsedCurrentIngredients = JSON.parse(currentIngredients);
+
+    parsedCurrentIngredients.needed.forEach(ingredient => {
+      appendIngredientToArray(ingredient.name, ingredient.quantity, ingredient.measure);
+      appendIngredientToView(ingredient.name, ingredient.quantity, ingredient.measure);
+    });
+  }
+
+  if (pictureInput) {
+    pictureInput.onchange = () => {
+      if (pictureInput.files.length > 0) {
+        const fileName = pictureInput.parentNode.querySelector(".file-name");
+        fileName.textContent = pictureInput.files[0].name;
+      }
+    }
+  }
+
+  if (stepsVideoInput) {
+    stepsVideoInput.onchange = () => {
+      if (stepsVideoInput.files.length > 0) {
+        const fileName = stepsVideoInput.parentNode.querySelector(".file-name");
+        fileName.textContent = stepsVideoInput.files[0].name;
+      }
+    }
+  }
+
+  if (ingredientAdder) {
+    ingredientAdder.addEventListener("click", (e) => {
+      appendIngredientToArray(ingredientSelect.value, ingredientAmount.value, ingredientMeasure.value);
+      appendIngredientToView(ingredientSelect.value, ingredientAmount.value, ingredientMeasure.value);
+    });
+  }
+
+  if (applianceAdder) {
+    applianceAdder.addEventListener("click", (e) => {
+      appendApplianceToArray(applianceSelector.value);
+      appendApplianceToView(applianceSelector.value);
+    });
+  }
+
+  if (theForm) {
+    theForm.addEventListener("submit", (e) => {
+      let ingredientJson = JSON.stringify({needed: selectedIngredients});
+      ingredientsInput.value = ingredientJson;  
+      appliancesInput.value = selectedAppliances;
+      return true;
+    });
+  }
+
+  if (isEditPath) {
+    if (isEditPath.value == "edit") {
+      sanitizeSteps();
+      loadAppliances();
+      loadIngredients();
+    }
+  }
 });
 
-applianceAdder.addEventListener("click", (e) => {
-  appendApplianceToArray();
-  appendApplianceToView();
-});
-
-theForm.addEventListener("submit", (e) => {
-  let ingredientJson = JSON.stringify({needed: selectedIngredients});
-  document.querySelector("#recipe_ingredients").value = ingredientJson;  
-  document.querySelector("#recipe_appliances").value = selectedAppliances;
-  return true;
-});
-
-const appendIngredientToArray = () => {
-  let newIngredient = {name:ingredientSelect.value, quantity:ingredientAmount.value, measure:ingredientMeasure.value };
-  selectedIngredients.push(newIngredient);  
-}
-
-const appendIngredientToView = () => {
+const appendIngredientToView = (newName, newAmount, newMeasure) => {
   let spanNode = document.createElement("span");
-  let innerText = document.createTextNode(`${ingredientSelect.value} ${ingredientAmount.value} ${ingredientMeasure.value}`);
+  let innerText = document.createTextNode(`${newName} ${newAmount} ${newMeasure}`);
   let deleteButton = document.createElement("button");
 
   deleteButton.setAttribute("type", "button");
-  deleteButton.setAttribute("id", `${ingredientSelect.value}_deleter`);
+  deleteButton.setAttribute("id", `${newName}_deleter`);
   deleteButton.classList.add("delete", "is-small", "ingredientDeleter");
 
   spanNode.classList.add("tag", "is-success", "is-light", "box", "active-ingredient");
@@ -63,18 +103,13 @@ const appendIngredientToView = () => {
   document.getElementById("ingredient_pool").appendChild(spanNode);
 }
 
-const appendApplianceToArray = () => {
-  let newAppliance = applianceInput.value;
-  selectedAppliances.push(newAppliance);
-}
-
-const appendApplianceToView = () => {
+const appendApplianceToView = (newAppliance) => {
   let spanNode = document.createElement("span");
-  let innerText = document.createTextNode(applianceInput.value);
+  let innerText = document.createTextNode(newAppliance);
   let deleteButton = document.createElement("button");
 
   deleteButton.setAttribute("type", "button");
-  deleteButton.setAttribute("id", `${applianceInput.value}_deleter`);
+  deleteButton.setAttribute("id", `${newAppliance}_deleter`);
   deleteButton.classList.add("delete", "is-small", "applianceDeleter");
 
   spanNode.classList.add("tag", "is-success", "is-light", "box", "active-appliance");
@@ -82,4 +117,14 @@ const appendApplianceToView = () => {
   spanNode.appendChild(deleteButton);
 
   document.getElementById("appliance_pool").appendChild(spanNode);
+}
+
+const sanitizeSteps = () => {
+  let stepsTextArea = document.querySelector("#recipe_steps");
+  let objectConstructor = ({}).constructor;
+
+  if (stepsTextArea.value === objectConstructor) {
+    stepsTextArea.value = JSON.parse(stepsTextArea.value).join("\n");
+  }
+  stepsTextArea.style.height = "auto";
 }
